1fcb7f19769f239c82bd4771bccc20b50a6a779c
Molin Liu <molin@live.cn>
2020-09-05 20:09:26 +0100

    Change equations in portfolio, change the gradient

--- vasicek_model.py
+++ vasicek_model.py
@@ -30,12 +30,13 @@ class Vasicek:
         self.initial_dW()
         
         # Calculate price under different measures
-        self.Tprice = self._cal_price(self.T, mode='P')
-        self.Sprice = self._cal_price(self.S, mode='P')
-        self.ZCB_price = self._cal_price(self.T/self.N, mode='P')
-        self.Uprice = self._cal_price(self.U, mode='P')
+        self.Tprice, self.dT, self.gamma_T = self._cal_price(self.T)
+        self.Sprice, self.dS, self.gamma_S = self._cal_price(self.S)
+        self.Uprice, self.dU, self.gamma_U = self._cal_price(self.U)
+        self.ZCB_price = self._cal_zcbprice()
 
         # Calculat delta T and delta S
+        '''
         self.dT = self._dx(self.Tprice)
         self.dS = self._dx(self.Sprice)
         self.dU = self._dx(self.Uprice)
@@ -44,7 +45,7 @@ class Vasicek:
         self.gamma_T = self._gamma_x(self.Tprice)
         self.gamma_S = self._gamma_x(self.Sprice)
         self.gamma_U = self._gamma_x(self.Uprice)
-
+        '''
 
         self.cal_quant_delta()
         self.cal_quant_gamma()
@@ -54,21 +55,23 @@ class Vasicek:
         self.state()
 
     def get_result(self):
-        var_s1 = np.percentile(self.pf_s1[-1], 95)/np.sqrt(180)
+        finalday_pf_1 = self.pf_s1[-1]
+        var_s1 = np.percentile(finalday_pf_1, 95)/np.sqrt(180)
         es_s1 = np.mean(var_s1)
-        pl_s1 = np.mean(self.pf_s1)
-        variance_s1 = np.mean(np.var(self.pf_s1, axis=0))
+        # Expected Profit & Loss
+        pl_s1 = np.mean(finalday_pf_1)
+        variance_s1 = np.mean(np.var(finalday_pf_1))
 
         var_s2 = np.percentile(self.pf_s2[-1], 95)/np.sqrt(180)
         es_s2 = np.mean(var_s2)
-        pl_s2 = np.mean(self.pf_s2)
-        variance_s2 = np.mean(np.var(self.pf_s2, axis=0))
+        pl_s2 = np.mean(self.pf_s2[-1])
+        variance_s2 = np.mean(np.var(self.pf_s2[-1]))
         
         result = {}
-        result['VaR'] = [var_s1, var_s2]
-        result['Expected Shortfall'] = [es_s1, es_s2]
-        result['Profit & loss'] = [pl_s1, pl_s2]
-        result['Variance'] = [variance_s1, variance_s2]
+        result['VaR'] = ["{:.4e}".format(var_s1), "{:.4e}".format(var_s2)]
+        result['Expected Shortfall'] = ["{:.4e}".format(es_s1), "{:.4e}".format(es_s2)]
+        result['Profit & loss'] = ["{:.4e}".format(pl_s1), "{:.4e}".format(pl_s2)]
+        result['Variance'] = ["{:.4e}".format(variance_s1), "{:.4e}".format(variance_s2)]
         
         self.result = pd.DataFrame(result)
 
@@ -84,7 +87,6 @@ class Vasicek:
         self.dW_P = np.random.randn(self.N, self.n_sim)
         #self.dW_P = self.sigma*self.lamb*self.dt+self.dW_Q
         self._generate_dr()
-        print(self.rate_P)
         #self.rate_P = self.rate_Qs
         return self.dW_Q, self.dW_P
         
@@ -94,25 +96,6 @@ class Vasicek:
         for i in range(1, self.N+1):
             epsilon = np.random.randn(1, self.n_sim)
             self.rate_P[i, ] = self.rate_P[i-1, ]+(self.kappa*(self.theta-self.rate_P[i-1, ])-self.lamb*self.sigma)*self.dt+self.sigma*np.sqrt(self.dt)*epsilon
-            #self._forward_rate_Q()
-            #self._forward_rate_P()
-    
-    def _forward_rate_P(self):
-        '''
-        dr_t = (k*(theta-r_{t})-lambda*sigma)+sigma*dWP[t]
-        '''
-        dr = (self.kappa*(self.theta-self.rate_P[-1])-self.lamb*self.sigma)+self.sigma*np.sqrt(self.dt)*self.dW_P[len(self.rate_P)-1,]
-        rt = self.rate_P[-1]+dr
-        self.rate_P = np.vstack((self.rate_P, rt))
-        
-    def _forward_rate_Q(self):
-        '''
-        Generate ideal dr from dWQ
-        drt = (k*(theta-r_{t})+sigma*dWQ[t])
-        '''
-        dr = (self.kappa*(self.theta-self.rate_Q[-1]))+self.sigma*np.sqrt(self.dt)*self.dW_Q[len(self.rate_Q)-1,]
-        rt = self.rate_Q[-1]+dr
-        self.rate_Q = np.vstack((self.rate_Q, rt))
     
     def _dx(self, x):
         return np.gradient(x, axis=0)
@@ -129,15 +112,48 @@ class Vasicek:
         Calculate the gamma of x.
         '''
         return self._dx(self._dx(x))
-    def _test_cal_price(self, due_date):
+    def _cal_price(self, due_date):
         T = due_date
         price = []
-        for t in np.arange(0, self.T, self.dt):
+        delta = []
+        gamma = []
+        for t in np.arange(0, self.T+self.dt, self.dt):
             A = self._A(t, T)
-            price_t = A*np.exp(-self._B(t, T)* self.rate_P[int(t/self.dt)])
+            B = self._B(t, T)
+            price_t = A*np.exp(-B* self.rate_P[int(t/self.dt)])
+            delta_t = -B*price_t
+            gamma_t = B*B*price_t
             price.append(price_t)
+            delta.append(delta_t)
+            gamma.append(gamma_t)
+
+        return np.array(price), np.array(delta), np.array(gamma)
+    def _cal_zcbprice(self):
+        B = (1/self.kappa)*(1-np.exp(-self.kappa*(self.dt)))
+        A = np.exp(
+            (
+                (self.theta-(np.power(self.sigma, 2)/(2*np.power(self.kappa, 2)))
+                -
+                (self.lamb*self.sigma/self.kappa)
+                )
+                *
+                (B-self.dt)
+            )
+            -
+            (
+                (
+                    (np.power(self.sigma, 2)/(4*self.kappa))*np.power(B, 2)
+                )
+            )
+        )
+        price = []
+        for t in np.arange(0, self.T+self.dt, self.dt):
+
+            temp_price = A*np.exp(-B*self.rate_P[int(t/self.dt)])
+            price.append(temp_price)
         return np.array(price)
-    def _cal_price(self, due_date, mode='P'):
+
+    def depercated_cal_price(self, due_date, mode='P'):
         '''
         Calculate prices with different due date
         '''
@@ -187,7 +203,8 @@ class Vasicek:
         qz[0,] = np.divide(self.Tprice[0]-self.Sprice[0]*self.quant_s_delta[0], self.ZCB_price[0])
 
         for i in range(1, self.N+1):
-            qz[i] = np.divide(self.Sprice[i-1]*self.quant_s_delta[i-1]-self.Sprice[i]*self.quant_s_delta[i]+qz[i-1], self.ZCB_price[i])
+            # Change here
+            qz[i] = np.divide(self.Sprice[i]*self.quant_s_delta[i-1]-self.Sprice[i]*self.quant_s_delta[i]+qz[i-1], self.ZCB_price[i])
         #self.qz[1:,] = np.divide(self.Sprice[:-1, ]*self.quant_s_delta[:-1, ]-self.Sprice[1:, ]*self.quant_s_delta[1:, ]+self.ZCB_price[:-1, ], self.ZCB_price[1:, ])
         
 
@@ -195,13 +212,22 @@ class Vasicek:
         self.qz_s1 = qz
     def portfolio_s2(self):
         qz = np.zeros((self.N+1, self.n_sim))
+        qS_s2 = np.divide(self.dU*self.gamma_T-self.dT*self.gamma_U, self.dU*self.gamma_S-self.dS*self.gamma_U)
+        qz[0] = np.divide(self.Tprice[0]-qS_s2[0]*self.Sprice[0]-self.quant_u_gamma[0]*self.Uprice[0], self.ZCB_price[0])
+        for i in range(1, self.N+1):
+            qz[i] = np.divide(qS_s2[i-1]*self.Sprice[i]+qz[i-1]+self.quant_u_gamma[i-1]*self.Uprice[i]-qS_s2[i]*self.Sprice[i]-self.quant_u_gamma[i]*self.Uprice[i], self.ZCB_price[i])
+        '''
+        Old version
+        Left here in case the test version doesn't work
         qz[0] = np.divide(self.Tprice[0]-self.Sprice[0]*self.quant_s_gamma[0]-self.Uprice[0]*self.quant_u_gamma[0], self.ZCB_price[0])
 
         for i in range(1, self.N+1):
             qz[i] = np.divide(self.Sprice[i-1]*self.quant_s_gamma[i-1]-self.Sprice[i]*self.quant_s_gamma[i]+qz[i-1]+self.Uprice[i-1]*self.quant_u_gamma[i]-self.Uprice[i]*self.quant_u_gamma[i], self.ZCB_price[i])
-
-        self.pf_s2 = -self.Tprice+self.quant_s_gamma*self.Sprice+self.Uprice*self.quant_u_gamma+qz*self.ZCB_price
+        '''
+        self.pf_s2 = -self.Tprice+qS_s2*self.Sprice+qz*self.ZCB_price+self.quant_u_gamma*self.Uprice
+        #self.pf_s2 = -self.Tprice+self.quant_s_gamma*self.Sprice+self.Uprice*self.quant_u_gamma+qz*self.ZCB_price
         self.qz_s2 = qz
+        self.qS_s2 = qS_s2
 
     def _A(self, t, T):
         result = np.exp(
@@ -221,28 +247,6 @@ class Vasicek:
             )
         )
         return result
-    
-    def _A_P(self, t, T):
-        return np.exp(
-            (
-                self.theta -
-                (np.power(self.sigma, 2)/(2*np.power(self.kappa, 2)))
-                - self.lamb*self.sigma/self.kappa
-            ) *
-            (self._B(t, T)-T+t) -
-            (np.power(self.sigma, 2)*0.25/self.kappa) *
-            np.power(self._B(t, T), 2)
-        )
-
-    def _A_Q(self, t, T):
-        return np.exp(
-            (
-                self.theta -
-                (np.power(self.sigma, 2)/(2*np.power(self.kappa, 2)))) *
-            (self._B(t, T)-T+t) -
-            (np.power(self.sigma, 2)*0.25/self.kappa) *
-            np.power(self._B(t, T), 2)
-        )
 
     def _B(self, t, T):
         '''
